f236531f8b92d0ec425cc9a52f665f7f2e764050
Sarutoh <sarutoh@outlook.com>
2020-04-23 21:38:24 +0300

    Add connection between User and Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_182008) do
+ActiveRecord::Schema.define(version: 2020_04_23_182723) do
 
   create_table "items", force: :cascade do |t|
     t.string "item"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
