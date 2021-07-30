f6e798a6c71607d1f6b63e1812eaaba20ff62caf
sarutoh <dovhaliukstepan@gmail.com>
2021-07-30 19:46:12 +0300

    remove autorization contexts from tests

--- spec/features/tasks/delete_spec.rb
+++ spec/features/tasks/delete_spec.rb
@@ -1,24 +1,22 @@
 # frozen_string_literal: true
 
 feature 'Delete task: ', type: :feature do
-  context 'With autorization' do
-    let(:current_user) { create(:user) }
-    let!(:task) { create(:task, user: current_user) }
+  let_it_be(:current_user) { create(:user) }
+  let_it_be(:task) { create(:task, user: current_user) }
 
-    before do
-      sign_in(current_user)
-      visit tasks_path
-    end
+  before do
+    sign_in(current_user)
+    visit tasks_path
+  end
 
-    scenario 'Page have delete button' do
-      expect(page).to have_content('Delete')
-      expect(page).to have_content('Complete')
-    end
+  scenario 'Page have delete button' do
+    expect(page).to have_link('Delete')
+    expect(page).to have_link('Complete')
+  end
 
-    scenario 'Succesfully delete task' do
-      expect { find_link('Delete').click }.to change { Task.count }.from(1).to(0)
-      expect(page).to have_content('Task was successfully destroyed.')
-      expect(current_path).to eq(tasks_path)
-    end
+  scenario 'Succesfully delete task' do
+    expect { find_link('Delete').click }.to change { Task.count }.from(1).to(0)
+    expect(page).to have_content('Task was successfully destroyed.')
+    expect(current_path).to eq(tasks_path)
   end
 end
--- spec/features/tasks/update_spec.rb
+++ spec/features/tasks/update_spec.rb
@@ -1,67 +1,65 @@
 # frozen_string_literal: true
 
 feature 'Update task: ', type: :feature do
-  context 'With autorization' do
-    let(:current_user) { create(:user) }
-    let!(:task) { create(:task, user: current_user) }
-    let(:success_message) { 'Task was successfully updated.' }
-    let(:error_message) { "Title is too short (minimum is #{Task::MIN_TITLE_LENGTH} characters)" }
-    let(:incorrect_value) { 'task1' }
-    let(:correct_value) { 'task_1' }
+  let_it_be(:current_user) { create(:user) }
+  let_it_be(:task) { create(:task, user: current_user) }
+  let_it_be(:success_message) { 'Task was successfully updated.' }
+  let_it_be(:error_message) { "Title is too short (minimum is #{Task::MIN_TITLE_LENGTH} characters)" }
+  let_it_be(:incorrect_value) { 'task1' }
+  let_it_be(:correct_value) { 'task_1' }
 
-    before { sign_in(current_user) }
+  before { sign_in(current_user) }
 
-    context 'update from root_path' do
-      before do
-        visit root_path
+  context 'update from root_path' do
+    before do
+      visit root_path
 
-        find_link('Edit').click
-      end
-
-      scenario 'Page have content update task' do
-        expect(page).to have_content('Editing Task')
-        expect(page).to have_content('Show items')
-        expect(page).to have_content('Back')
-        expect(current_path).to eq("/tasks/#{task.id}/edit")
-      end
-
-      scenario 'Successfully update task' do
-        within('form') do
-          fill_in 'task_title', with: correct_value
-
-          click_button 'Update'
-        end
-
-        expect(page).to have_content(correct_value)
-        expect(page).to have_content(success_message)
-        expect(current_path).to eq(root_path)
-      end
-
-      scenario 'Do not update task' do
-        within('form') do
-          fill_in 'task_title', with: incorrect_value
-
-          click_button 'Update'
-        end
-
-        expect(page).to have_content(error_message)
-        expect(current_path).to eq("/tasks/#{task.id}")
-      end
+      find_link('Edit').click
     end
 
-    context 'update from individual task view' do
-      before do
-        visit "tasks/#{task.id}"
+    scenario 'Page have content update task' do
+      expect(page).to have_content('Editing Task')
+      expect(page).to have_link('Show items')
+      expect(page).to have_link('Back')
+      expect(current_path).to eq(edit_task_path(task))
+    end
 
-        find_link('Edit').click
+    scenario 'Successfully update task' do
+      within('form') do
+        fill_in 'task_title', with: correct_value
+
+        click_button 'Update'
       end
 
-      scenario 'Page have content update task' do
-        expect(page).to have_content('Editing Task')
-        expect(page).to have_content('Show items')
-        expect(page).to have_content('Back')
-        expect(current_path).to eq("/tasks/#{task.id}/edit")
+      expect(page).to have_content(correct_value)
+      expect(page).to have_content(success_message)
+      expect(current_path).to eq(root_path)
+    end
+
+    scenario 'Do not update task' do
+      within('form') do
+        fill_in 'task_title', with: incorrect_value
+
+        click_button 'Update'
       end
+
+      expect(page).to have_content(error_message)
+      expect(current_path).to eq(task_path(task))
+    end
+  end
+
+  context 'update from individual task view' do
+    before do
+      visit task_path(task)
+
+      find_link('Edit').click
+    end
+
+    scenario 'Page have content update task' do
+      expect(page).to have_content('Editing Task')
+      expect(page).to have_link('Show items')
+      expect(page).to have_link('Back')
+      expect(current_path).to eq(edit_task_path(task))
     end
   end
 end
